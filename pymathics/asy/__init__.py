a90a83600531efe5ba4d26691e864c0d7514b0cb
mmatera <matera@fisica.unlp.edu.ar>
2021-02-25 10:08:16 -0300

    setting up tests.

--- pymathics/asy/__init__.py
+++ pymathics/asy/__init__.py
@@ -9,8 +9,11 @@ This module provides a replacement for the graphics subsystem using asymptote to
 import os
 from pymathics.asygraphics.version import __version__
 from pymathics.asygraphics.__main__ import Hello # noqa
-
-__all__ = ("__version__", "Graphics", "pymathics_version_data")
+from pymathics.asy.exportasy import (ExportToJPG,
+                                     ExportToPNG,
+                                     ExportToSVG,
+                                     ExportToPDF)
+__all__ = ("__version__", "ExportToJPG", "ExportToPNG", "ExportToSVG", "ExportToPDF", "pymathics_version_data")
 
 # To be recognized as an external mathics module, the following variable
 # is required:
