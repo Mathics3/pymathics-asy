b5308e64d08ca4198a53587d5ef1d010af1b5862
mmatera <matera@fisica.unlp.edu.ar>
2021-03-02 10:27:57 -0300

    adding graphics

--- pymathics/asy/__init__.py
+++ pymathics/asy/__init__.py
@@ -6,6 +6,7 @@ This module provides a replacement for the graphics subsystem using asymptote to
 
 """
 
+import pkg_resources
 import os
 
 from pymathics.asy.version import __version__
@@ -13,13 +14,33 @@ from pymathics.asy.exportasy import (ExportToJPG,
                                      ExportToPNG,
                                      ExportToSVG,
                                      ExportToPDF)
+from pymathics.asy.graphics import (AsyGraphicsBox,
+                                    AsyGraphics3DBox)
+
 from mathics.builtin.importexport import Export as MathicsExport
-__all__ = ("__version__", "ExportToJPG", "ExportToPNG", "ExportToSVG", "ExportToPDF", "pymathics_version_data")
+__all__ = ("__version__", "ExportToJPG", "ExportToPNG", "ExportToSVG", "ExportToPDF",
+           "AsyGraphicsBox", "AsyGraphics3DBox", "pymathics_version_data")
+
+
+ROOT_DIR = pkg_resources.resource_filename('pymathics.asy', '')
+
+
+
+def onload(definitions):
+    print("onload Asy")
+    from mathics.builtin import box_constructs
+    box_constructs["System`GraphicsBox"] = AsyGraphicsBox
+    box_constructs["System`Graphics3DBox"] = AsyGraphics3DBox
+    MathicsExport._extdict["pdf"] = "PDF"
+    
+    for root, dirs, files in os.walk(os.path.join(ROOT_DIR, "autoload")):
+        for path in [os.path.join(root, f) for f in files if f.endswith(".m")]:
+                    Expression("Get", String(path)).evaluate(Evaluation(self))
 
 # To be recognized as an external mathics module, the following variable
 # is required:
 #
-MathicsExport._extdict["pdf"] = "PDF"
+
 
 
 
@@ -27,4 +48,5 @@ pymathics_version_data = {
     "author": "The Mathics Team",
     "version": __version__,
     "requires": [],
+    "onload" : onload
 }
