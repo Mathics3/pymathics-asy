fafeb51f4650adcfd75c4be339afd278f47098f5
mmatera <matera@fisica.unlp.edu.ar>
2021-04-08 21:01:39 -0300

    fix

--- test/test_asy.py
+++ test/test_asy.py
@@ -4,9 +4,19 @@
 import os
 import pymathics.asy
 
+
+import shutil
+try:
+    asy_path = shutil.which("asy")
+    print("asy found in ", asy_path)
+except:
+    print("asy path couldn't be found. Set to asy")
+    asy_path = "asy"
+
+
 def test_asycmd():
     try:
-        assert os.system(pymathics.asy.asy_path +" -version") == 0
+        assert os.system(asy_path +" -version") == 0
     except:
-        print("path=", pymathics.asy.asy_path)
+        print("path=", asy_path)
         assert False
